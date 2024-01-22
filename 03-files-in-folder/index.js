78164c7832614f41800556e8d7ca7d47f3bb1b8b
Aleksei Arkhangelskii <arhmost@gmail.com>
2024-01-22 20:26:43 +0100

    feat: add output all files with stat task 3

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,10 +4,18 @@ const path = require('path');
 const secretFolderPath = path.join(__dirname, 'secret-folder');
 
 fs.readdir(secretFolderPath, { withFileTypes: true }, (err, entries) => {
+  entries.forEach((entry) => {
+    const filePath = path.join(secretFolderPath, entry.name);
 
-    const files = entries.filter(entry => entry.isFile());
-
-    files.forEach(file => {
-      console.log(file.name);
+    fs.stat(filePath, (err, stats) => {
+      if (err) {
+        return;
+      }
+      if (stats.isFile()) {
+        const fileExtension = path.extname(entry.name, '.');
+        const fileName = path.basename(entry.name, fileExtension);
+        console.log(`${fileName} - ${fileExtension.slice(1)} - ${stats.size}b`);
+      }
     });
   });
+});
