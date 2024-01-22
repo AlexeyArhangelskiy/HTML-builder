e9ccedb4c207825c0145c8d4a29fc64db413071e
Aleksei Arkhangelskii <arhmost@gmail.com>
2024-01-23 00:57:50 +0100

    fear: add func task 6 and fix task5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,34 +1,38 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const stylesFolderPath = path.join(__dirname, 'styles');
 const outputFolderPath = path.join(__dirname, 'project-dist');
 const outputFilePath = path.join(outputFolderPath, 'bundle.css');
-
-function readStylesFolder(folderPath) {
+async function readStylesFolder(folderPath) {
   try {
-    const files = fs.readdirSync(folderPath, { withFileTypes: true });
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
     const styles = [];
-    files.forEach((file) => {
+
+    for (const file of files) {
       const filePath = path.join(folderPath, file.name);
+
       if (file.isFile() && path.extname(filePath) === '.css') {
-        const content = fs.readFileSync(filePath, 'utf8');
+        const content = await fs.readFile(filePath, 'utf8');
         styles.push(content);
       }
-    });
+    }
     return styles;
   } catch (error) {
     console.error('error', error.message);
     return [];
   }
 }
-function writeBundleFile(styles) {
+async function writeBundleFile(styles) {
   try {
     const bundleContent = styles.join('\n');
-    fs.writeFileSync(outputFilePath, bundleContent, 'utf8');
+    await fs.writeFile(outputFilePath, bundleContent, 'utf8');
   } catch (error) {
     console.error('error', error.message);
   }
 }
-const styles = readStylesFolder(stylesFolderPath);
-writeBundleFile(styles);
+async function run() {
+  const styles = await readStylesFolder(stylesFolderPath);
+  await writeBundleFile(styles);
+}
+run();
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,9 +5,82 @@ async function buildPage() {
   try {
     const outputFolderPath = path.join(__dirname, 'project-dist');
     await fs.mkdir(outputFolderPath, { recursive: true });
-    console.log('successfully');
+    const templateFilePath = path.join(__dirname, 'template.html');
+    let templateContent = await fs.readFile(templateFilePath, 'utf8');
+
+    const tagNames = templateContent
+      .match(/{{(.*?)}}/g)
+      .map((tag) => tag.slice(2, -2).trim());
+
+    for (const tagName of tagNames) {
+      const componentFilePath = path.join(
+        __dirname,
+        'components',
+        `${tagName}.html`,
+      );
+      const componentContent = await fs.readFile(componentFilePath, 'utf8');
+      templateContent = templateContent.replace(
+        new RegExp(`{{${tagName}}}`, 'g'),
+        componentContent,
+      );
+    }
+    const outputFilePath = path.join(outputFolderPath, 'index.html');
+    await fs.writeFile(outputFilePath, templateContent, 'utf8');
+    const stylesFolderPath = path.join(__dirname, 'styles');
+    const styles = await readStylesFolder(stylesFolderPath);
+    const bundleContent = styles.join('\n');
+    const styleOutputPath = path.join(outputFolderPath, 'style.css');
+    await fs.writeFile(styleOutputPath, bundleContent, 'utf8');
+    const assetsSrcPath = path.join(__dirname, 'assets');
+    const assetsDestPath = path.join(outputFolderPath, 'assets');
+    await copyDir(assetsSrcPath, assetsDestPath);
   } catch (error) {
     console.error('Error', error.message);
   }
 }
+async function readStylesFolder(folderPath) {
+  try {
+    const files = await fs.readdir(folderPath, { withFileTypes: true });
+    const styles = [];
+
+    for (const file of files) {
+      const filePath = path.join(folderPath, file.name);
+
+      if (file.isFile() && path.extname(filePath) === '.css') {
+        const content = await fs.readFile(filePath, 'utf8');
+        styles.push(content);
+      }
+    }
+    return styles;
+  } catch (error) {
+    console.error('error', error.message);
+    return [];
+  }
+}
+async function copyDir(src, dest) {
+  try {
+    const destExists = await fs
+      .access(dest)
+      .then(() => true)
+      .catch(() => false);
+    if (destExists) {
+      await fs.rm(dest, { recursive: true });
+    }
+    await fs.mkdir(dest, { recursive: true });
+    const files = await fs.readdir(src);
+    for (const file of files) {
+      const srcPath = path.join(src, file);
+      const destPath = path.join(dest, file);
+      const stat = await fs.stat(srcPath);
+      if (stat.isDirectory()) {
+        await copyDir(srcPath, destPath);
+      } else {
+        await fs.copyFile(srcPath, destPath);
+      }
+    }
+  } catch (error) {
+    console.error('Error', error.message);
+  }
+}
+
 buildPage();
