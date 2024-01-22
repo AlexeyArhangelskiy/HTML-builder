399e0de1188e6e46a6f848a1da5eeadd3a643e6f
Aleksei Arkhangelskii <arhmost@gmail.com>
2024-01-22 22:22:47 +0100

    fear: add func task 4

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyDir(src, dest) {
+  try {
+    await fs.mkdir(dest, { recursive: true });
+    const files = await fs.readdir(src);
+    for (const file of files) {
+      const srcPath = path.join(src, file);
+      const destPath = path.join(dest, file);
+      const stat = await fs.stat(srcPath);
+      if (stat.isDirectory()) {
+        await copyDir(srcPath, destPath);
+      } else {
+        await fs.copyFile(srcPath, destPath);
+      }
+    }
+  } catch (error) {
+    console.error('Error', error.message);
+  }
+}
+const srcPath = path.join(__dirname, 'files');
+const destPath = path.join(__dirname, 'files-copy');
+copyDir(srcPath, destPath);
